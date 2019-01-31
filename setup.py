ebea9670a1c5925e84f49e47cda78b543c79fbdd
Francesca Capel <capel.francesca@gmail.com>
2019-01-31 10:29:56 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = 'vMF',
+    packages = setuptools.find_packages(),
+    version = 'v0.1.0',
+    description = 'von Mises - Fisher distribution',
+    author = 'Francesca Capel',
+    author_email = 'capel.francesca@gmail.com',
+    url = 'https://github.com/cescalara/vMF',
+    keywords = ['statistics'],
+    classifiers = [],
+)
